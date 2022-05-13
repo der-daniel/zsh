7a594cd156d519cc6435cf07571c54e968d2d035
Daniel Hitzel <der-daniel@outlook.com>
2022-05-13 08:41:52 +0200

    Add link-plugins.sh

---
+++ link-plugins.sh
@@ -0,0 +1,4 @@
+ln -s ~/Repositories/zsh/autoupdate-oh-my-zsh-plugins ~/.oh-my-zsh/custom/plugins/autoupdate
+ln -s ~/Repositories/zsh/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ln -s ~/Repositories/zsh/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+ln -s ~/Repositories/zsh/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
